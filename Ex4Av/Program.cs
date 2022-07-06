daf5ce00ee5e1eb93e6c07db3daf51c925251020
Recepção 2 <Recepo 2@RECEPO2>
2022-07-06 16:07:36 -0300

    Adicionar arquivos de projeto.

---
+++ Ex4Av/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4Av
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //4 – Utilizando estrutura FOR, escrever um programa para exibir os números de 1 até 50 na tela.
+            int c;
+            for (c = 1; c < 51; c++)
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
